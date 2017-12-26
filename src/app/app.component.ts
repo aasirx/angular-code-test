9f24fccfa4cf3b262e61d8e99419f6f92acc1330
= <li.yi101@zte.com.cn>
2017-12-26 22:35:37 +0800

    添加jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var jquery: any; declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  onClick() {
+    console.log('onclick');
+  }
+  ngOnInit() {
+    $('#img').click();
+  }
 }
